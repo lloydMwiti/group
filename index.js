5d4f7d39231e93fd1c08cd7687be8d88208f5886
lloydM <lloydMwiti4@gmail.com>
2020-04-15 22:43:33 +0300

    minor changes to layout

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const searchbar = document.forms['.cards'].querySelector('.src');
+function srcbox(){
+    const input,filter,ul,li,a,i,txtvalue;
+    input=document.getElementById('srcbar');
+    filter=input.value.toLowerCase();
+    ul=document.getElementById('srclist');
+    li=document.getElementsByTagName('li');
 
-searchbar.addEventListener('keyup',function(e){
-    const term=e.target.value.toLowerCase();
-    const movies=list.getElementsByclassName('content');
-    Array.from(movies).forEach(function(movies){
-        const title=movies.firstElementChild()
-    })
-})
+    for(i=0 ;i<li.lenght;i++){
+        a=li[i].getElementsByTagName("li")[0];
+        txtvalue=a.textContent || a.innerText;
+        if(txtvalue.toLowerCase().indexOf(filter)> -1){
+            li[i].style.display="";
+        }
+        else{
+            li[i].style.display="none";
+        }
+    }
+}
