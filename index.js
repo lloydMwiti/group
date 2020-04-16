4fb0f42eddd34f54e12b94808a4096bbaa51d394
lloydM <lloydMwiti4@gmail.com>
2020-04-16 09:21:24 +0300

    initial'

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function srcbox(){
         a=li[i].getElementsByTagName("li")[0];
         txtvalue=a.textContent || a.innerText;
         if(txtvalue.toLowerCase().indexOf(filter)> -1){
-            li[i].style.display="";
+            li[i].style.display="nothing";
         }
         else{
             li[i].style.display="none";
